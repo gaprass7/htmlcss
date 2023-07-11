2b3adea955da95ddb2fdf9b789fc22e868f65548
gaprass7 <gnngzr@gmail.com>
2023-07-11 21:21:02 +0700

    p

---
+++ main.js
@@ -0,0 +1,43 @@
+console.log("ya"); 
+
+// function formcoba (e){
+//     e.preventDefault()
+//     const inputValue = e.target.children
+//     console.log(inputValue)
+// }
+// let var1=10,var2="10"
+
+// if (var1==var2){
+//     console.log("if benar")
+// } else if (var1===var2){
+//     console.log("else if benar")
+// } else{
+//     console.log("salah")
+// }
+
+// if (var1 === var2) {
+//   console.log("else if benar");
+// } else {
+//   console.log("salah");
+// }
+
+// function perkalian(num1, num2){
+//     console.log(num1 * num2)
+// }
+// perkalian(5, 10)
+
+
+// let el= document.getElementById("navbar")
+// console.log(el)
+
+
+const btnShow = $("#show")
+const btnHide = $("#hide")
+
+btnShow.click(function{
+    div.show()
+})
+
+btnHide.click(function{
+    div.hide()
+})
